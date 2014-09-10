da6563f0c762c6e7741bdf8c47d455963d269ebd
FUJI Goro (gfx) <gfuji@cpan.org>
2014-09-11 07:43:06 +0900

    Initial Commit

---
+++ JsonSerializer/JsonSerializer.h
@@ -0,0 +1,19 @@
+//
+//  JsonSerializer.h
+//  JsonSerializer
+//
+//  Created by Fuji Goro on 2014/09/11.
+//  Copyright (c) 2014年 Fuji Goro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JsonSerializer.
+FOUNDATION_EXPORT double JsonSerializerVersionNumber;
+
+//! Project version string for JsonSerializer.
+FOUNDATION_EXPORT const unsigned char JsonSerializerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JsonSerializer/PublicHeader.h>
+
+
