bc45bbb443b51ac7f5eaf6f0b18d015a72f8806a
FUJI Goro (gfx) <gfuji@cpan.org>
2014-09-17 22:56:38 +0900

    add an example project: SwiftFeed

---
+++ SwiftFeed/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  SwiftFeed
+//
+//  Created by Fuji Goro on 2014/09/17.
+//  Copyright (c) 2014年 Fuji Goro. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail: AnyObject = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
