8ee577a4cfb236c6e8a753a3abe2b146f7445539
FUJI Goro (gfx) <gfuji@cpan.org>
2014-09-21 10:57:30 +0900

    tweaks

--- SwiftFeed/DetailViewController.swift
+++ SwiftFeed/DetailViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 import JsonSerializer
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UIWebViewDelegate {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var webView: UIWebView!
 
     var detailItem: Json!
 
@@ -24,9 +24,18 @@ class DetailViewController: UIViewController {
     func configureView() {
         title = detailItem["title"].stringValue
 
-        let detail = self.detailItem
-        let label = self.detailDescriptionLabel
-        label.text = detail.debugDescription
+        let url = NSURL(string: detailItem["link"].stringValue)
+        let request = NSURLRequest(URL: url)
+
+        webView.loadRequest(request)
+        webView.delegate = self
+    }
+
+    var indicator: OverlayIndicator?
+    func webViewDidStartLoad(webView: UIWebView) {
+        indicator = OverlayIndicator()
+    }
+    func webViewDidFinishLoad(webView: UIWebView) {
+        indicator = nil
     }
 }
-
