fa5ad64e82fa2e31b0e139a3339e07008dc6e2a3
Logan Wright <logan.william.wright@gmail.com>
2016-05-10 01:15:36 -0400

    gitignore

--- .build/versionData/PureJson.swift
+++
@@ -1,5 +0,0 @@
-public let url: String = "/Users/loganwright/Dropbox/Programming/xCode/Swift-PureJsonSerializer"
-public let version: (major: Int, minor: Int, patch: Int, prereleaseIdentifiers: [String], buildMetadata: String?) = (2, 0, 0, [], nil)
-public let versionString: String = "2.0.0"
-public let sha: String? = nil
-public let modified: Bool = false
--- .build/versionData/PureJsonSerializer.swift
+++
@@ -1,5 +0,0 @@
-public let url: String = "/Users/loganwright/Dropbox/Programming/xCode/Swift-PureJsonSerializer"
-public let version: (major: Int, minor: Int, patch: Int, prereleaseIdentifiers: [String], buildMetadata: String?) = (1, 2, 0, [], nil)
-public let versionString: String = "1.2.0"
-public let sha: String? = "af0187c9a99c6b4bc6517c569f63218db70e1e79"
-public let modified: Bool = true
